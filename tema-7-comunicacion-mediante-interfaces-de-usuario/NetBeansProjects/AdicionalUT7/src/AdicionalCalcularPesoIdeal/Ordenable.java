4c50822a43bc441673347295065c7931d3f579d1
Kevin <kevinhg94@gmail.com>
2019-03-16 16:27:25 +0000

    Tema7-InicioTemaInterfazGraficaDeUsuarios

---
+++ tema-7-comunicacion-mediante-interfaces-de-usuario/NetBeansProjects/AdicionalUT7/src/AdicionalCalcularPesoIdeal/Ordenable.java
@@ -0,0 +1,23 @@
+/*
+    Práctica 17: Crear la interfaz: Ordenable
+        e incluir como métodos: boolean
+        esMayorQue() , esMenorQue(), esIgualQue() Implementar el interfaz en la clase Persona
+        (tomaremos la clase abstracta que ya hemos usado en esta unidad ) ¿ qué ocurre en el IDE
+        cuándo escribimos: Persona implements Ordenable ? ( tomar captura de pantalla )
+        mediante alt+enter hacer el override
+ */
+package AdicionalCalcularPesoIdeal;
+
+
+
+
+
+/**
+ *
+ * @author Kevin Hernández García <kevinhg94@gmail.com>
+ */
+public interface Ordenable<T> {
+    boolean esMayorQue(T elemento);
+    boolean esMenorQue(T elemento);
+    boolean esIgualQue(T elemento);
+}
